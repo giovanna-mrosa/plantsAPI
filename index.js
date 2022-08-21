6912db1279744ee5d83513409d7e3ac586380607
giovanna-mrosa <gmarcaro@gmail.com>
2022-08-21 16:13:25 -0300

    fix: allow cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv').config()
 const { DB_URL } = process.env
 
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 const app = express()
 
@@ -14,6 +15,15 @@ app.use(
 )
 
 app.use(express.json())
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 const plantRoutes = require('./routes/plantRoutes')
 
